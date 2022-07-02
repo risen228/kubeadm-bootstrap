7da5ef209dc7b24fbac619c3ab0448f3fb900773
Evgeny Zakharov <risenforces@gmail.com>
2022-07-02 16:39:08 +0300

    feat: add prepare

---
+++ prepare.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+swapoff -a
+
+# apply basic ufw settings
+ufw default deny incoming
+ufw default allow outgoing
+
+# allow ssh
+ufw allow ssh
+
+# allow control-plane connections
+ufw allow 6443
+ufw allow 2379:2380
+ufw allow 10250
+ufw allow 10259
+ufw allow 10257
+
+# allow worker connections
+ufw allow 10250
+ufw allow 30000:32767
+
+# enable ufw
+ufw --force enable
+
+echo "Prepare script executed. Run \"reboot\" command to apply settings."
---
+++ ubuntu-18.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+FLANNEL_CIDR="10.224.0.0/16"
+CRI_SOCKET="/var/run/crio/crio.sock"
+
