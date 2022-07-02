6406fb32ea1be513a890c162bd0df569af6a199b
Evgeny Zakharov <risenforces@gmail.com>
2022-07-02 19:23:05 +0300

    fix

--- init-kubeadm.sh
+++ init-kubeadm.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 OS="xUbuntu_18.04"
-CRIO_VERSION="1.24"
-FLANNEL_CIDR="10.224.0.0/16"
+CRI_VERSION="1.24"
 CRI_SOCKET="/var/run/crio/crio.sock"
+FLANNEL_CIDR="10.224.0.0/16"
 
 # add repositories
 echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$CRIO_VERSION/$OS/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
+echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$CRI_VERSION/$OS/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$CRI_VERSION.list
 
 # add gpg keys for repositories
-curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION/$OS/Release.key | apt-key add -
+curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$CRI_VERSION/$OS/Release.key | apt-key add -
 curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | apt-key add -
 
 # install cri-o
