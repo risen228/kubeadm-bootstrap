2c0f20f5f0bac30081a41d9a6b891b122f5c44b1
Evgeny Zakharov <risenforces@gmail.com>
2022-07-02 20:36:48 +0300

    fix

--- init-kubeadm.sh
+++ init-kubeadm.sh
@@ -5,17 +5,19 @@ CRI_VERSION="1.24"
 CRI_SOCKET="/var/run/crio/crio.sock"
 FLANNEL_CIDR="10.224.0.0/16"
 
+apt-get update
+apt-get install -y gnupg
+
 # add repositories
 echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$CRI_VERSION/$OS/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$CRI_VERSION.list
 
-# add gpg keys for repositories
+# add gpg key
 curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$CRI_VERSION/$OS/Release.key | apt-key add -
 curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | apt-key add -
 
-# install cri-o
-sudo apt update
-sudo apt -y install cri-o cri-o-runc
+apt-get update
+apt-get install -y cri-o cri-o-runc
 
 # bootstrap cri-o
 sudo systemctl enable crio.service
